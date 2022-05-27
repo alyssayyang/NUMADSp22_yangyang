4c591d531b3e03be29489b17ea9ac81d4b3d2c2c
YANG YANG <yang.yang16@northeastern.edu>
2022-05-26 20:20:58 -0400

    FINAL COMMIT

---
+++ app/src/main/java/edu/neu/myapplication/Activity2.java
@@ -0,0 +1,49 @@
+package edu.neu.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
+import android.os.Bundle;
+import android.widget.GridLayout;
+import android.widget.TextView;
+
+public class Activity2 extends AppCompatActivity {
+
+    private GridLayout g;
+    private Button button15;
+    private Button button17;
+    private Button button18;
+    private Button button19;
+    private Button button20;
+    private Button button21;
+    private TextView tv2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+        g = (GridLayout) findViewById(R.id.gridLayout2);
+
+        button15 = (Button)findViewById(R.id.button15);
+        button17 = (Button)findViewById(R.id.button17);
+        button18 = (Button)findViewById(R.id.button18);
+        button19 = (Button)findViewById(R.id.button19);
+        button20 = (Button)findViewById(R.id.button20);
+        button21 = (Button)findViewById(R.id.button21);
+
+        tv2 = (TextView) findViewById(R.id.textView2);
+    }
+
+    public void onBtnClicked(View v){
+        v.getId();
+        tv2.setText("Pressed: " + ((Button)v).getText());
+    }
+
+
+
+
+
+}
--- app/src/main/java/edu/neu/myapplication/MainActivity.java
+++ app/src/main/java/edu/neu/myapplication/MainActivity.java
@@ -1,14 +1,56 @@
+/*
+YANG YANG
+CS 5520 A1
+MAY 13 2022
+ */
+
 package edu.neu.myapplication;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActicity2();
+            }
+        });
+
+
     }
+
+    public void displayToastMsg(String msg){
+        Toast toast = Toast.makeText(this,msg,Toast.LENGTH_LONG);
+        //toast.setGravity(Gravity.FILL_HORIZONTAL,0,0);
+        toast.show();
+    }
+
+    public void intro(View v){
+        displayToastMsg("YANG YANG \nyang.yang16@northeastern.edu");
+    }
+
+    public void openActicity2(){
+        Intent intent = new Intent(this,Activity2.class);
+        startActivity(intent);
+    }
+
+
 }
