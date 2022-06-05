3184b1e22cd4b7bdebbd1bd7f078716877b0c679
YANG YANG <yang.yang16@northeastern.edu>
2022-06-05 17:45:05 -0400

    finish recycle vew

--- app/src/main/java/edu/neu/myapplication/Activity3.java
+++ app/src/main/java/edu/neu/myapplication/Activity3.java
@@ -6,6 +6,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
@@ -13,6 +17,11 @@ public class Activity3 extends AppCompatActivity {
 
     private ArrayList<links> linkslist;
     private RecyclerView recyclerView;
+    private FloatingActionButton enterNewLink;
+
+    private TextView textViewName;
+    private TextView textViewURL;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +29,23 @@ public class Activity3 extends AppCompatActivity {
         setContentView(R.layout.activity_3);
 
         recyclerView = findViewById(R.id.recyclerView);
+        enterNewLink = findViewById(R.id.enterNewLink);
         linkslist = new ArrayList<>();
+
+        enterNewLink.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                openDialog();
+            }
+        });
 
         setLinksInfo();
         setAdapter();
 
+
     }
 
-    private void setAdapter(){
+    private void setAdapter() {
         recyclerAdapter adapter = new recyclerAdapter(linkslist);
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
@@ -37,8 +55,20 @@ public class Activity3 extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
     }
 
-    private void setLinksInfo(){
-        linkslist.add(new links("google","https://www.google.com.hk/?client=safari&gws_rd=ssl"));
+    private void setLinksInfo() {
+        linkslist.add(new links("google", "https://www.google.com.hk/?client=safari&gws_rd=ssl"));
     }
 
+    public void openDialog(){
+        //creating an instance of dialog
+        EnterLinkDialog enterLinkDialog = new EnterLinkDialog();
+        enterLinkDialog.show(getSupportFragmentManager(),"Enter New Link");
+    }
+
+
+
+
+
+
 }
+
